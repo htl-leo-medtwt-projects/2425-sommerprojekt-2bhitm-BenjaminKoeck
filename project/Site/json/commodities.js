1efdf10ec9234949e60f14671201716c9fcf6835
BenjaminKoeck <b.koeck@students.htl-leonding.ac.at>
2025-05-14 20:18:14 +0200

    Refactor commodities data structure; update path references to obj in commodities.js and enhance calculator script functionality in calcScript.js; adjust script loading order in calc.html

--- project/Site/json/commodities.js
+++ project/Site/json/commodities.js
@@ -6,7 +6,7 @@ let commodities = [
             {
                 "name": "ArcCorp Mining Area 141",
                 "location": "Daymar, Crusader, Stanton",
-                "path": "locations.stanton.crusader.satellites.daymar.acma141",
+                "obj": locations.stanton.crusader.satellites.daymar.acma141,
                 "price": 2076.0
             }
         ],
@@ -14,31 +14,31 @@ let commodities = [
             {
                 "name":"Admin Office",
                 "location": "Grim HEX, Yela, Crusader, Stanton",
-                "path": "locations.stanton.crusader.satellites.yela.satellites.grimHex.adminOffice",
+                "obj": locations.stanton.crusader.satellites.yela.satellites.grimHEX.adminOffice,
                 "price": 2266.0
             },
             {
                 "name": "Admin Office",
                 "location": "HUR-L3 Thundering Express Station, HUR-L3, Stanton",
-                "path": "locations.stanton.hurston.lagrange.hurL3.adminOffice",
+                "obj": locations.stanton.hurston.lagrange.hurL3.adminOffice,
                 "price": 2502.0
             },
             {
                 "name": "Admin Office",
                 "location": "HUR-L4 Melodic Fields Station, HUR-L4, Stanton",
-                "path": "locations.stanton.hurston.lagrange.hurL4.adminOffice",
+                "obj": locations.stanton.hurston.lagrange.hurL4.adminOffice,
                 "price": 2505.0
             },
             {
                 "name": "Brio's Braker Yard",
                 "location": "Brio's Braker Yard, Daymar, Crusader, Stanton",
-                "path": "locations.stanton.crusader.satellites.daymar.bby",
+                "obj": locations.stanton.crusader.satellites.daymar.bby,
                 "price": 2265.0
             },
             {
                 "name": "Transfers Commodity Exchange",
                 "location": "Central Business District, Lorville, Hurston, Stanton",
-                "path": "locations.stanton.hurston.lorville.tce",
+                "obj": locations.stanton.hurston.surface.lorville.tce,
                 "price": 2391.0
             }
         ]
@@ -50,31 +50,31 @@ let commodities = [
             {
                 "name": "Picker's Field",
                 "location": "Hurston, Stanton",
-                "path": "locations.stanton.hurston.surface.pickersField",
+                "obj": locations.stanton.hurston.surface.pickersField,
                 "price": 9774.0
             },
             {
                 "name": "Platinum Bay",
                 "location": "HUR-L2 Faithful Dream Station, HUR-L2, Stanton",
-                "path": "locations.stanton.hurston.lagrange.hurL2.platinumBay",
+                "obj": locations.stanton.hurston.lagrange.hurL2.platinumBay,
                 "price": 15000.0
             },
             {
                 "name": "Platinum Bay",
                 "location": "HUR-L3 Thundering Express Station, HUR-L3, Stanton",
-                "path": "locations.stanton.hurston.lagrange.hurL3.platinumBay",
+                "obj": locations.stanton.hurston.lagrange.hurL3.platinumBay,
                 "price": 15000.0
             },
             {
                 "name": "Platinum Bay",
                 "location": "HUR-L5 High Course Station, HUR-L5, Stanton",
-                "path": "locations.stanton.hurston.lagrange.hurL5.platinumBay",
+                "obj": locations.stanton.hurston.lagrange.hurL5.platinumBay,
                 "price": 15000.0
             },
             {
                 "name": "Rappel",
                 "location": "Hurston, Stanton",
-                "path": "locations.stanton.hurston.surface.rappel",
+                "obj": locations.stanton.hurston.surface.rappel,
                 "price": 9775.0
             }
         ],
@@ -82,31 +82,31 @@ let commodities = [
             {
                 "name":"Admin Office",
                 "location": "Grim HEX, Yela, Crusader, Stanton",
-                "path": "locations.stanton.crusader.satellites.yela.satellites.grimHex.adminOffice",
+                "obj": locations.stanton.crusader.satellites.yela.satellites.grimHEX.adminOffice,
                 "price": 9265.0
             },
             {
                 "name": "Brio's Braker Yard",
                 "location": "Brio's Braker Yard, Daymar, Crusader, Stanton",
-                "path": "locations.stanton.crusader.satellites.daymar.bby",
+                "obj": locations.stanton.crusader.satellites.daymar.bby,
                 "price": 7800.0
             }, 
             {
                 "name": "Reclamation and Disposal Orinth",
                 "buys": "Hurston, Stanton",
-                "path": "locations.stanton.hurston.surface.rdo",
+                "obj": locations.stanton.hurston.surface.rdo,
                 "price": 8450.0
             },
             {
                 "name": "Terra Mills HydroFarm",
                 "location": "Cellin, Crusader, Stanton",
-                "path": "locations.stanton.crusader.satellites.cellin.tmhf",
+                "obj": locations.stanton.crusader.satellites.cellin.tmhf,
                 "price": 11072.0
             },
             {
                 "name": "Transfers Commodity Exchange",
                 "location": "Central Business District, Lorville, Hurston, Stanton",
-                "path": "locations.stanton.hurston.lorville.tce",
+                "obj": locations.stanton.hurston.surface.lorville.tce,
                 "price": 8500.0
             }
 
--- project/Site/script/calcScript.js
+++ project/Site/script/calcScript.js
@@ -25,16 +25,88 @@ let outputs = {
     "ntPrftPH": document.getElementById("netProfitPerHour")
 };
 
+
+let selectValues = {
+    "commodity": null,
+    "origin": null,
+    "destination": null,
+    "ship": null
+};
+
+let inputValues = {
+    "prchsPrc": null,
+    "sellPrc": null,
+    "qty": null,
+    "expnss": null,
+    "flgtTime": null
+};
+
+
+loadCalc();
+
+if (selectValues.commodity != null) {
+    selectValues.origin = selectValues.commodity.buyableAt[parseInt(selects.originInp.value)];
+    selectValues.destination = selectValues.commodity.sellableAt[parseInt(selects.destInp.value)];
+    selectValues.ship = ships[parseInt(selects.shipInp.value)];
+}
+
+
+
 function calc() {
-    let inputValues = {
-        "prchsPrc": null,
-        "sellPrc": null,
-        "qty": null,
-        "expnss": null,
-        "flgtTime": null
+
+    if (selectValues.commodity != null && selectValues.origin != null && selectValues.destination != null && selectValues.ship != null) {
+
+    }
+}
+
+function loadCalc() {
+    let output = "";
+
+    selectValues.commodity = commodities[parseInt(selects.cmdtyInp.value)];
+
+    if (selects.cmdtyInp.value != '') {
+        output += `<option value="" disabled selected>Origin</option>`
+        for (let i = 0; i < selectValues.commodity.buyableAt.length; i++) {
+            output += `<option value="${i}">${selectValues.commodity.buyableAt[i].name}</option>`;
+        }
+        selects.originInp.innerHTML = output;
+        output = "";
+
+
+        output += `<option value="" disabled selected>Destination</option>`;
+        for (let i = 0; i < selectValues.commodity.sellableAt.length; i++) {
+            output += `<option value="${i}">${selectValues.commodity.sellableAt[i].name}</option>`;
+        }
+        selects.destInp.innerHTML = output;
+        output = "";
+
+        /* output += `<option value="" disabled selected>Ship</option>`;
+        for (let i = 0; i < selectValues.ships.length; i++) {
+            output += `<option value="${i}">${selectValues.ships[i].name}</option>`;
+        }
+        selects.shipInp.innerHTML = output;
+        output = ""; */
+    } else {
+        output = "";
+
+        output += `<option value="" disabled selected>Commodity</option>`;
+        for (let i = 0; i < commodities.length; i++) {
+            output += `<option value="${i}">${commodities[i].name}</option>`;
+        }
+        selects.cmdtyInp.innerHTML = output;
+        output = "";
     }
 
-    if (selects.cmdtyInp.value != null && selects.originInp.value != 0) {
-        
+    if (selectValues.origin != null) {
+        inputValues.prchsPrc = selectValues.origin.price;
+        inputs.prchsPrcInp.value = inputValues.prchsPrc;
+    }
+    if (selectValues.destination != null) {
+        inputValues.sellPrc = selectValues.destination.price;
+        inputs.sellPrcInp.value = inputValues.sellPrc;
+    }
+    if (selectValues.ship != null) {
+        inputValues.qty = parseInt(selects.qtyInp.value);
+        inputs.qtyInp.value = inputValues.qty;
     }
 }
